e7e333a45551da0ddb7e1ce67fca8829198f0bd8
VIDHITTS <vidhitts@gmail.com>
2025-04-24 15:19:45 +0530

    added shadcn and necessary files for it

---
+++ site.config.js
@@ -0,0 +1,10 @@
+/** @type {import("shadcn-ui").Config} */
+module.exports = {
+    $schema: "https://ui.shadcn.com/schema.json",
+    name: "markdown-magic",
+    outDir: "./src/components/ui",
+    componentDefaults: {
+      className: "rounded-2xl shadow-md"
+    }
+  };
+  
