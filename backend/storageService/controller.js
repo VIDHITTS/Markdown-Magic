00ca5980fedd4c7b16855ea9c75799b00c1f5732
VIDHITTS <vidhitts@gmail.com>
2025-11-26 21:50:16 +0530

    added two more routes get specific code and delete code

--- backend/storageService/controller.js
+++ backend/storageService/controller.js
@@ -7,7 +7,7 @@ const creator = async (req, res) => {
         const project = await prisma.project.create({
             data: {
                 title,
-                description : description ? description : "",
+                description: description ? description : "",
                 isPublic: isPublic ? isPublic : false,
                 userId: req.user.userId
             }
@@ -19,18 +19,52 @@ const creator = async (req, res) => {
     }
 }
 
-const getallCodes = async (req,res)=>{
-    try{
+const getallCodes = async (req, res) => {
+    try {
         const projects = await prisma.project.findMany({
-            where:{
+            where: {
                 userId: req.user.userId
             }
         })
         return res.status(200).json({ message: "Projects fetched successfully", projects: projects })
-    } catch(error){
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+const getCodebyId = async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (!id) {
+            return res.status(400).json({ message: "Code id is required" });
+        }
+        const project = await prisma.project.findUnique({
+            where: {
+                id: id
+            }
+        })
+        return res.status(200).json({ message: "Codes fetched successfully", project: project })  
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+}   
+const deleteCode = async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (!id) {
+            return res.status(400).json({ message: "Code id is required" });
+        }
+        const project = await prisma.project.delete({
+            where: {
+                id: id
+            }
+        })
+        return res.status(200).json({ message: "Codes deleted successfully"})  
+    } catch (error) {
         console.log(error);
         return res.status(500).json({ message: "Internal Server Error" });
     }
 }
 
-module.exports={creator,getallCodes}
+module.exports = { creator, getallCodes, getCodebyId,deleteCode }
--- backend/storageService/router.js
+++ backend/storageService/router.js
@@ -1,10 +1,15 @@
 const express = require("express");
 
-const { creator,getallCodes } = require("./controller.js");
+const { creator,getallCodes,getCodebyId,deleteCode,updateCode } = require("./controller.js");
 const { middleware } = require("./middleware");
+
 const router = express.Router();
+
 router.use(middleware)
+
 router.post("/create",creator)
 router.get("/all",getallCodes)
-
+router.get("/:id",getCodebyId) 
+router.delete("/:id",deleteCode)
+router.put("/:id",updateCode)
 module.exports = router;    
