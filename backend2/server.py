519e0980657df2fc931308ae48a7857f75fe7db4
VIDHITTS <vidhitts@gmail.com>
2025-12-16 00:25:14 +0530

    changed

--- backend2/server.py
+++ backend2/server.py
@@ -37,7 +37,7 @@ app.include_router(feed_router)
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello World!"}
+    return {"message": "Heo World!"}
 
 if __name__ == "__main__":
     import uvicorn
